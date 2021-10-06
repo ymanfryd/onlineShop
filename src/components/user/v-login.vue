233fe2eff468f26760dc3edfdf4079e051920869
arigova <arigova@smartdatatech.io>
2021-10-06 17:06:29 +0300

    registration added

---
+++ src/components/user/v-login.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="v-login">
+  <form action="submit">
+    <input type="text" placeholder="email">
+    <input type="text" placeholder="password">
+    <button type="submit">sign in</button>
+    <p></p>
+  </form>
+</div>
+</template>
+
+<script>
+export default {
+  name: "v-login"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/user/v-register.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "v-register"
+}
+</script>
+
+<style scoped>
+
+</style>
